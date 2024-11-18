f22c9bbe875545b10d063e2aaea2eac3f8d4da65
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2024-11-18 16:33:04 -0300

    add partial content

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,3 +1,133 @@
+import {
+  Calendar,
+  CircleCheck,
+  CircleDashed,
+  MapPin,
+  Plus,
+  Settings2,
+  UserCog,
+} from "lucide-react";
+import { useState } from "react";
+import { CreateActivityModal } from "./create-activity-modal";
+import { ImportantLinks } from "./important-links";
+
 export function TripDetailsPage() {
-  return <h1>Trip details page</h1>;
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
+    useState(false);
+
+  function openCreateActivityModal() {
+    setIsCreateActivityModalOpen(true);
+  }
+
+  function closeCreateActivityModal() {
+    setIsCreateActivityModalOpen(false);
+  }
+
+  return (
+    <div className="max-w-[1100px] mx-auto px-6 py-10 space-y-8">
+      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <MapPin className="size-5 text-zinc-400" />
+          <span className=" text-zinc-100">Florianópolis, Brasil</span>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <span className="text-zinc-100">17 a 23 de agosto</span>
+          </div>
+          <div className="w-px h-6 bg-zinc-800" />
+          <button className="flex items-center gap-2 bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium hover:bg-zinc-700 transition-colors">
+            Alterar local/data
+            <Settings2 className="size-5" />
+          </button>
+        </div>
+      </div>
+
+      <main className="flex gap-16 px-4">
+        <div className="flex-1 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-semibold">Atividades</h2>
+            <button
+              onClick={openCreateActivityModal}
+              className="flex items-center gap-2 bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium hover:bg-lime-400 transition-colors"
+            >
+              <Plus className="size-5 text-lime-950" />
+              Cadastrar atividade
+            </button>
+          </div>
+
+          <div className="space-y-8">
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">
+                  Dia 17
+                </span>
+                <span className="text-xs text-zinc-500">Sábado</span>
+              </div>
+
+              <p className="text-sm text-zinc-500">
+                Nenhuma atividade cadastrada nessa data.
+              </p>
+            </div>
+
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">
+                  Dia 18
+                </span>
+                <span className="text-xs text-zinc-500">Domingo</span>
+              </div>
+
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                  <CircleCheck className="size-5 text-lime-300" />
+                  <span className="text-zinc-100">Academia em grupo</span>
+                  <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <aside className="w-80 space-y-6">
+          <ImportantLinks />
+
+          <div className="w-full h-px bg-zinc-800" />
+
+          <div className="space-y-6 ">
+            <h2 className="font-semibold text-xl">Convidados</h2>
+
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5 overflow-hidden">
+                  <span className="block font-medium text-zinc-100">
+                    Jessica White
+                  </span>
+                  <span
+                    title="jessica.white@yahoo.com"
+                    className="block text-sm text-zinc-400 truncate"
+                  >
+                    jessica.white@yahoo.com
+                  </span>
+                </div>
+                <CircleDashed className="size-5 text-zinc-400 shrink-0" />
+              </div>
+            </div>
+
+            <button className="w-full flex items-center justify-center gap-2 bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium hover:bg-zinc-700 transition-colors">
+              <UserCog className="size-5" />
+              Gerenciar convidados
+            </button>
+          </div>
+        </aside>
+      </main>
+
+      {isCreateActivityModalOpen && (
+        <CreateActivityModal
+          closeCreateActivityModal={closeCreateActivityModal}
+        />
+      )}
+    </div>
+  );
 }
