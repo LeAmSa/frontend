6bf719382aea7dc34b9f7eb73c3f2b5e58e9158f
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2024-11-21 17:46:36 -0300

    create ModalTitle

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,34 +1,54 @@
-import { ReactNode } from "react";
+import { ComponentProps, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
-interface ModalRootProps {
+interface ModalRootProps extends ComponentProps<"div"> {
   children: ReactNode;
   onClose: () => void;
 }
 
-interface ModalContentProps {
+interface ModalContentProps extends ComponentProps<"div"> {
   children: ReactNode;
 }
 
-function ModalRoot({ children, onClose }: ModalRootProps) {
+interface ModalTitleProps extends ComponentProps<"h2"> {
+  text: string;
+}
+
+function ModalRoot({ children, onClose, ...props }: ModalRootProps) {
   return (
     <div
       className="fixed inset-0 bg-black/60 flex items-center justify-center"
       onClick={(e) => e.target === e.currentTarget && onClose()}
+      {...props}
     >
       {children}
     </div>
   );
 }
 
-function ModalContent({ children }: ModalContentProps) {
+function ModalContent({ children, ...props }: ModalContentProps) {
   return (
-    <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+    <div
+      className={twMerge(
+        "w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5",
+        props.className
+      )}
+    >
       {children}
     </div>
+  );
+}
+
+function ModalTitle({ text, ...props }: ModalTitleProps) {
+  return (
+    <h2 className="text-left text-lg font-semibold" {...props}>
+      {text}
+    </h2>
   );
 }
 
 export const Modal = {
   Root: ModalRoot,
   Content: ModalContent,
+  Title: ModalTitle,
 };
