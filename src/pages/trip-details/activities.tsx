74aedfb62151667c586f1f529d4780b5005bd94a
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2025-01-13 16:10:35 -0300

    create logic to list activities

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,33 +1,64 @@
 import { CircleCheck } from "lucide-react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale/pt-BR";
+
+interface Activity {
+  date: string;
+  activities: {
+    id: string;
+    title: string;
+    occurs_at: string;
+  }[];
+}
 
 export function Activities() {
+  const { tripId } = useParams();
+  const [activities, setActivities] = useState<Activity[] | undefined>();
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/activities`)
+      .then((response) => setActivities(response.data.activities));
+  }, [tripId]);
+
   return (
     <div className="space-y-8">
-      <div className="space-y-2.5">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
-          <span className="text-xs text-zinc-500">Sábado</span>
-        </div>
+      {activities?.map((activity, index) => (
+        <div key={index} className="space-y-2.5">
+          <div className="flex gap-2 items-baseline">
+            <span className="text-xl text-zinc-300 font-semibold">
+              Dia {format(activity.date, "d")}
+            </span>
+            <span className="text-xs text-zinc-500">
+              {format(activity.date, "EEEE", { locale: ptBR })}
+            </span>
+          </div>
 
-        <p className="text-sm text-zinc-500">
-          Nenhuma atividade cadastrada nessa data.
-        </p>
-      </div>
-
-      <div className="space-y-2.5">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
-          <span className="text-xs text-zinc-500">Domingo</span>
-        </div>
-
-        <div className="space-y-2.5">
-          <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-            <CircleCheck className="size-5 text-lime-300" />
-            <span className="text-zinc-100">Academia em grupo</span>
-            <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+          <div className="space-y-2.5">
+            {activity.activities.length === 0 ? (
+              <p className="text-sm text-zinc-500">
+                Nenhuma atividade cadastrada nessa data.
+              </p>
+            ) : (
+              activity.activities.map((act) => (
+                <div
+                  key={act.id}
+                  className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3"
+                >
+                  <CircleCheck className="size-5 text-lime-300" />
+                  <span className="text-zinc-100">{act.title}</span>
+                  <span className="text-zinc-400 text-sm ml-auto">
+                    {format(act.occurs_at, "HH:mm")}
+                  </span>
+                </div>
+              ))
+            )}
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
